687dc65f02c40e7f4500cd5032d968d087e594f3
Hugo.V <hugo.vialaneix@gmail.com>
2025-02-03 11:33:14 +0100

    main fini

--- Project2/exo3.c
+++ Project2/exo3.c
@@ -4,7 +4,17 @@
 
 
 int main() {
-	
+	int nb_note = 12;
+	float frequencies[12] = { 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99,
+	  392.00, 415.30, 440.00, 466.16, 493.88 };
+	Note** notes = malloc(nb_note * sizeof(Note));
+	for (int i = 0; i < nb_note; i++) {
+		notes[i]->frequence = frequencies[i];
+		notes[i] = creerNote();
+	}
+	simulerNote(notes, nb_note);
+	libererNote(notes);
+	return 0;
 
 }
 
